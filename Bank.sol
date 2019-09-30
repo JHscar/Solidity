6afe50a7b5e43d220e67bd473d00a6cb95893c3c
Lee Jae Hun <wogns5206@naver.com>
2019-10-01 03:59:52 +0900

    Solidity_example

---
+++ Bank.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.8;
+
+contract Bank {
+    uint public totalDeposit;
+    mapping(address => uint) balanceOf;
+
+    constructor() public {
+
+    }
+
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
+        totalDeposit += msg.value;
+    }
+
+    function withdraw(uint _amount) public {
+        balanceOf[msg.sender] -= _amount;
+        totalDeposit -= _amount;
+        msg.sender.transfer(_amount)
+    }
+
+    function getBalanceOf(address _account) public view returns (uint) {
+        return balanceOf[_account];
+    }
+
+}
